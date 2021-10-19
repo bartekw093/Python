c9e1b570c7fecc953823e2ee793423586b1b15c6
bartekw093 <34055196+bartekw093@users.noreply.github.com>
2021-10-19 23:12:46 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,34 @@
-def hello(imie):
-    print(f'Witaj, {imie}')
-    
-hello('bartek')
+import random
+import getpass as hide
+
+
+print('choose game mode!')
+print('1. VS computer')
+print('2. Vs player')
+game = int(input("Enter number of game mode: "))
+if game == 1: #Player versus Computer
+    number = random.randint(1,10)
+    guess = 0
+    counter = 0
+    while number != guess:
+        guess = int(input('have a try: '))
+        if guess==number:
+            print(f'Congratulations, you won! it only took you {counter} tries')
+            break
+        else:
+            counter = counter + 1
+            print(f'I\'m Sorry you failed again... for the {counter} time')
+elif game == 2: #Player versus player
+    print('Player 1 please enter number between 1 and 10 in pasword field')
+    hiden = hide.getpass()
+    hiden = int(hiden)
+    guess = 0
+    counter = 0
+    while hiden != guess:
+        guess = int(input('have a try: '))
+        if guess==hiden:
+            print(f'Congratulations, you won! it only took you {counter} tries')
+            break
+        else:
+            counter = counter + 1
+            print(f'I\'m Sorry you failed again... for the {counter} time')
